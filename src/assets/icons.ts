b9817f56242d17deef9aa251e74569cacf7d4ebf
oyejin0925 <142577684+oyejin0925@users.noreply.github.com>
2026-01-07 16:53:13 +0900

    [#3] nav바 제작

---
+++ src/assets/icons.ts
@@ -0,0 +1,14 @@
+//로고
+import Logo2 from "./icons/Logo2.svg?react";
+import Booklog from "./icons/Booklog.svg?react";
+
+//시스템 아이콘 
+import Alarm from "./icons/Alarm.svg?react";
+import Back from "./icons/Back.svg?react";
+
+export {
+  Logo2,
+  Alarm,
+  Back,
+  Booklog
+};
