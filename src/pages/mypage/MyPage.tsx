1946c0bcb56f10e1eae6516f253e2d32d7b19a49
oyejin0925 <142577684+oyejin0925@users.noreply.github.com>
2026-01-18 15:20:18 +0900

    [#82] 마이 설정페이지 퍼블리싱

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -5,14 +5,27 @@ import ReadingStatus from "../../components/mypage/ReadingStatus";
 import ReadingRanking from "../../components/mypage/ReadingRanking";
 import ReadingCalendar from "../../components/mypage/ReadingCalendar";
 import NavbarBottom from "../../components/common/navbar/NavBarBottom";
+import { useNavigate } from "react-router-dom";
 
 function MyPage() {
+  const navigate = useNavigate();
+
+  const handleSettingClick = () => {
+    navigate("/setting");
+  };
+
   return (
     <div className="min-h-screen bg-bg">
       <main className="mx-auto flex min-h-screen max-w-4xl flex-col">
         <header className="mt-4 px-5 flex h-15 items-center justify-between">
           <p className="text-head">마이페이지</p>
-          <Setting className="h-6 w-6 text-black" />
+          <button
+            onClick={handleSettingClick}
+            className="p-1 hover:opacity-70 transition-opacity"
+            aria-label="설정"
+          >
+            <Setting className="h-6 w-6 text-black" />
+          </button>
         </header>
 
         <section className=" px-5">
