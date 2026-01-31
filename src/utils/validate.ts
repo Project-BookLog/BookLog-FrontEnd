4cfc31461ec1e174b78e176135bd3e541c831948
shin-jaehyun <tlswogus0225@g.hongik.ac.kr>
2026-01-31 22:34:50 +0900

    [#169] fix: 버그 수정

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -11,7 +11,7 @@ function validateUser(values: UserLoginInformation) {
     
 
     if (
-        !/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i.test(
+        !/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,}$/i.test(
         values.email,
         )
     ) {
